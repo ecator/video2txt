697b37aff026bcc5ceaf31b0616dbd9c8b7be41a
Martin <qule520@126.com>
2026-01-30 17:11:49 +0800

    chore: 温度值设为1（kimi-2.5模型必须要是1）

--- sample/rename-conan-tv.ps1
+++ sample/rename-conan-tv.ps1
@@ -83,7 +83,7 @@ foreach ($file in $files) {
     Write-Host "  resolution: $resolution"
     Write-Host "  coder: $coder"
     Write-Host "  lang: $lang"
-    $newFileName = uv run --directory $Video2TxtPath main.py -f $file.FullName -p $prompt -a -s 60 -e 300
+    $newFileName = uv run --directory $Video2TxtPath main.py -f $file.FullName -p $prompt -a -s 60 -e 300 -t 1
     if($? -eq $false -or $newFileName -eq "") {
         Write-Host -ForegroundColor Yellow "  Get title failed, skip"
         continue
